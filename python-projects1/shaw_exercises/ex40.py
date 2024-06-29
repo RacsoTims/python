5157d7e570d6d9215cbc3b0c1b41b2aa64b495d1
RacsoTims <oscar-850@hotmail.com>
2024-06-29 18:39:50 +0200

    python-reorganization

--- python-projects1/shaw_exercises/ex40.py
+++ python-projects1/shaw_exercises/ex40.py
@@ -4,8 +4,11 @@ class Song(object):
         self.lyrics = lyrics
     
     def sing_me_a_song(self):
-        for line in self.lyrics:
-            print(line)
+        return self.lyrics
+    
+    def add_line(self, line):
+        self.lyrics.append(line)
+        return True
     
 happy_bday = Song(["\tHappy birthday to you",
                     "I don't want to get sued",
@@ -25,10 +28,15 @@ horse_with_no_name = Song(["\tYou see I have been to the desert on a horse with 
                             "'cause there ain't no one for to give you no pain...",
                             "---------------------"])
 
-happy_bday.sing_me_a_song()
+# happy_bday.sing_me_a_song()
 
-bulls_on_parade.sing_me_a_song()
+# bulls_on_parade.sing_me_a_song()
 
-horse_with_no_name.sing_me_a_song()
+lyric = horse_with_no_name.sing_me_a_song()
+for line in lyric:
+    print(line)
+
+if horse_with_no_name.add_line("la la la la la la la la la "):
+    print(horse_with_no_name.sing_me_a_song())
 
 # text.sing_me_a_song()
